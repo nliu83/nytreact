593e1ca6c8c107a1f6b4645ebdfd1e64b8b8273c
nliu83 <nliu8338@gmail.com>
2018-10-11 00:38:32 -0700

    fixes file structure

---
+++ client/src/components/Articles/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Articles from "./component/Articles";
+import Nav from "./components/Nav";
+
+const App = () => (
+  <div>
+    <Nav />
+    <Articles />
+  </div>
+);
+
+export default App;
